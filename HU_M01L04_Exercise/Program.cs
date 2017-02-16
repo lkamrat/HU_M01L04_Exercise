e2916f79781b888f2b5c21c291a50209c5f4d2d2
lkamrat <kamrat9@gmail.com>
2017-02-16 10:50:59 +0200

    Q1

--- HU_M01L04_Exercise/Program.cs
+++ HU_M01L04_Exercise/Program.cs
@@ -10,6 +10,19 @@ namespace HU_M01L04_Exercise
     {
         static void Main(string[] args)
         {
+            // Show the number if it's bigger then X or smaller then Y 
+            Console.WriteLine("Please enter an integer");
+            int integerA = int.Parse(Console.ReadLine());
+            int bigNum = 938;
+            int smallNum = 532;
+            if ((integerA > bigNum) || (integerA < smallNum))
+            {
+                Console.WriteLine(integerA + " is bigger then " + bigNum + " and smaller then " + smallNum);
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
         }
     }
 }
